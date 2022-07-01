efa28a080aab2566912800a6b54620ff2b46262b
muztaba <hasanat.muztaba@gmail.com>
2022-07-01 02:46:43 -0500

    Security Init

---
+++ lab5/src/main/java/com/waa/lab5/repo/UserRepository.java
@@ -0,0 +1,12 @@
+package com.waa.lab5.repo;
+
+import com.waa.lab5.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
---
+++ lab5/src/main/java/com/waa/lab5/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.waa.lab5.security;
+
+import com.waa.lab5.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new UserDetail(user);
+    }
+
+}
