71959d420df06c0e858334c6bbf0963a15ef7997
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-06-30 22:06:34 -0500

    Removed Unmanaged Refrences

--- spring_JWT/src/main/java/com/example/spring_jwt/entity/Product.java
+++ spring_JWT/src/main/java/com/example/spring_jwt/entity/Product.java
@@ -1,7 +1,5 @@
 package com.example.spring_jwt.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- spring_JWT/src/main/java/com/example/spring_jwt/entity/Review.java
+++ spring_JWT/src/main/java/com/example/spring_jwt/entity/Review.java
@@ -1,6 +1,5 @@
 package com.example.spring_jwt.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
 
 import javax.persistence.*;
