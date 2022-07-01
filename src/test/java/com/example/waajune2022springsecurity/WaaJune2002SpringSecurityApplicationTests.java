146eb7fa868704c62376f851a2e7a1a3143371f7
Sardor979bek <37942959+Sardor979bek@users.noreply.github.com>
2022-07-01 05:47:23 -0500

    Done

--- src/test/java/com/example/waajune2022springsecurity/WaaJune2002SpringSecurityApplicationTests.java
+++ src/test/java/com/example/waajune2022springsecurity/WaaJune2002SpringSecurityApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.waajune2002springsecurity;
+package com.example.waajune2022springsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
