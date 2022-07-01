793b59bc0b4a760c6b1cec4a39afc890a7854485
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-06-30 22:25:08 -0500

    Added Sql Data

--- spring_JWT/src/main/resources/data.sql
+++ spring_JWT/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (id, email, firstname, lastname, password)
 VALUES (1, 'apokhrel@miu.edu', 'ashish', 'pokhrel', '$2a$12$IKEQb00u5QpZMx4v5zMweu.3wrq0pS7XLCHO4yHZ.BW/yvWu1feo2'); --123
 INSERT INTO users (id, email, firstname, lastname, password)
-VALUES (2, 'admin@miu.edu', 'admin', 'user', '$2a$12$IKEQb00u5QpZMx4v5zMweu.3wrq0pS7XLCHO4yHZ.BW/yvWu1feo2'); --123
+VALUES (2, 'puskar@miu.edu', 'puskar', 'budhatoki', '$2a$12$IKEQb00u5QpZMx4v5zMweu.3wrq0pS7XLCHO4yHZ.BW/yvWu1feo2'); --123
 
 INSERT INTO role (id, role)
 VALUES (1, 'ADMIN');
